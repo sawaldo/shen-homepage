3884f6336e3423471504aa21fa005dd22599abfe
Sheni <sawaldo7@gmail.com>
2024-01-30 16:39:51 +0100

    Changed Image to Img from chakra-ui

--- components/thought.js
+++ components/thought.js
@@ -1,5 +1,5 @@
 import NextLink from 'next/link'
-import { Heading, Box, Image, Link, Badge } from '@chakra-ui/react'
+import { Heading, Box, Img, Link, Badge } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
 
 export const Title = ({ children }) => (
@@ -18,7 +18,7 @@ export const Title = ({ children }) => (
 )
 
 export const ThoughtImage = ({ src, alt }) => (
-  <Image borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
+  <Img borderRadius="lg" w="full" src={src} alt={alt} mb={4} />
 )
 
 export const Meta = ({ children }) => (
